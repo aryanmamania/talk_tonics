4159645fd634dff7b98561bf888fda8201599efd
Aryan Mamania <aryanmamania17@gmail.com>
2025-02-03 23:16:27 +0530

    working on chatpage

--- src/routes/chatpage/Chatpage.jsx
+++ src/routes/chatpage/Chatpage.jsx
@@ -2,8 +2,44 @@ import React from 'react'
 
 const Chatpage = () => {
   return (
-    <div>Chatpage</div>
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test Message</div>
+           <div className="message-user">Test message from user</div>
+           <div className="message">Test Message from ai</div>
+           <div className="message-user">Test message from user</div> 
+
+
+           <div className="message">Test Message</div>
+           <div className="message-user">Test message from user</div>
+           <div className="message">Test Message from ai</div>
+           <div className="message-user">Test message from user</div> 
+
+
+           <div className="message">Test Message</div>
+           <div className="message-user">Test message from user</div>
+           <div className="message">Test Message from ai</div>
+           <div className="message-user">Test message from user</div> 
+
+
+           <div className="message">Test Message</div>
+           <div className="message-user">Test message from user</div>
+           <div className="message">Test Message from ai</div>
+           <div className="message-user">Test message from user</div> 
+
+
+           <div className="message">Test Message</div>
+           <div className="message-user">Test message from user</div>
+           <div className="message">Test Message from ai</div>
+           <div className="message-user">Test message from user</div> 
+
+        </div>
+      </div>
+    </div>
   )
 }
+
+1 hour 14 min 30 sec 
 
 export default Chatpage
